d79b94ef557b26d34dc83389433e485f2945e2d3
14555269625852 <sveta-maricheva@mail.ru>
2024-07-27 21:44:47 +0300

    add second homework

--- lesson_5/Chrome/Click  button  CSS class.py
+++ lesson_5/Chrome/Click  button  CSS class.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+from selenium.common.exceptions import NoSuchElementException
 
 chrome = webdriver.Chrome()
 chrome.get("http://uitestingplayground.com/classattr")
 
 sleep(5)
 for x in range(1, 4):
-    blue_button = chrome.find_element(
-        By.CSS_SELECTOR, ("button[class='btn btn-primary']")).click()
-    alert = chrome.switch_to.alert
-    alert.accept()
-print(x)
+    try:
+        blue_button = chrome.find_element(
+            By.CSS_SELECTOR, ("button[class='btn btn-primary']")).click()
+        alert = chrome.switch_to.alert
+        alert.accept()
+    except NoSuchElementException:
+        print(x)
 
 chrome.quit()
--- lesson_5/Firefox/Click  button  CSS class.py
+++ lesson_5/Firefox/Click  button  CSS class.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+from selenium.common.exceptions import NoSuchElementException
 
 firefox = webdriver.Firefox()
 firefox.get("http://uitestingplayground.com/classattr")
 
-sleep(2)
+sleep(5)
 for x in range(1, 4):
-    blue_button = firefox.find_element(
-        By.CSS_SELECTOR, ('button[class="btn btn-primary"]')).click()
-    alert = firefox.switch_to.alert
-    alert.accept()
-print(x)
+    try:
+        blue_button = firefox.find_element(
+            By.CSS_SELECTOR, ("button[class='btn btn-primary']")).click()
+        alert = firefox.switch_to.alert
+        alert.accept()
+    except NoSuchElementException:
+        print(x)
 
 firefox.quit()
